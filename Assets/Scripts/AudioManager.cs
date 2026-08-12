548adfc2de25f7b26bbc512d0065af901c5f76fb
Absalon43 <absalonpaaske@gmail.com>
2026-08-13 00:35:15 +0200

    added piss and shit
    UI sound effects

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,7 +12,14 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private EventReference UI_onButtonClicked;
     [SerializeField] private EventReference UI_onButtonClickedFailure;
     [SerializeField] private EventReference UI_onButtonClickedSlider;
+    [SerializeField] private EventReference UI_onStartButtonClicked;
+    [SerializeField] private EventReference UI_onStageSelect;
+    [SerializeField] private EventReference UI_charSelectSword;
+    [SerializeField] private EventReference UI_charSelectReaper;
+    [SerializeField] private EventReference UI_charSelectTwinblade;
+    [SerializeField] private EventReference UI_charSelectAxeman;
     [SerializeField] private FMOD.Studio.EventInstance musicEvent;
+    
 
     private void Awake()
     {
@@ -38,7 +45,7 @@ public class AudioManager : MonoBehaviour
     {
         AudioManager.instance.PlayOneShot(UI_onButtonClicked, this.transform.position);
     }
-    public void playButtonOnClickedFailure()
+    public void PlayButtonOnClickedFailure()
     {
         AudioManager.instance.PlayOneShot(UI_onButtonClickedFailure, this.transform.position);
     }
@@ -46,7 +53,30 @@ public class AudioManager : MonoBehaviour
     {
         AudioManager.instance.PlayOneShot(UI_onButtonClickedSlider, this.transform.position);
     }
-
+    public void PlayStartButtonClicked()
+    {
+        AudioManager.instance.PlayOneShot(UI_onStartButtonClicked, this.transform.position);
+    }
+    public void PlayStageSelect()
+    {
+        AudioManager.instance.PlayOneShot(UI_onStageSelect, this.transform.position);
+    }
+    public void PlayCharSelectSword()
+    {
+        AudioManager.instance.PlayOneShot(UI_charSelectSword, this.transform.position);
+    }
+    public void PlayCharSelectReaper()
+    {
+        AudioManager.instance.PlayOneShot(UI_charSelectReaper, this.transform.position);
+    }
+    public void PlayCharSelectTwinblade()
+    {
+        AudioManager.instance.PlayOneShot(UI_charSelectTwinblade, this.transform.position);
+    }
+    public void PlayCharSelectAxeman()
+    {
+        AudioManager.instance.PlayOneShot(UI_charSelectAxeman, this.transform.position);
+    }
     public void MusicVolume()
     {
         
