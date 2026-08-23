5cb1245d37e71832b5721c348adbc57155f81ef6
Marcelkress <marcel@kress.dk>
2026-08-23 20:23:52 +0200

    hallo man det ser bare nice ud nu

--- Assets/Scripts/SettingsPanel.cs
+++ Assets/Scripts/SettingsPanel.cs
@@ -1,10 +1,6 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using FMOD;
-using FMOD.Studio;
-using UnityEngine.Rendering;
 
 public class SettingsPanel : MonoBehaviour
 {
@@ -25,6 +21,7 @@ public class SettingsPanel : MonoBehaviour
         ChangeMusicVolume(AudioValues.instance.musicVol);
         ChangeAmbientVolume(AudioValues.instance.ambienceVol);
         ChangeCharacterVolume(AudioValues.instance.CharVol);
+        Screen.SetResolution(2560, 1440, true);
     }
 
     public void OnEnter()
@@ -106,13 +103,13 @@ public class SettingsPanel : MonoBehaviour
         switch (val)
         {
             case 0:
-                Screen.SetResolution(1920, 1080, true);
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
                 break;
             case 1:
-                Screen.SetResolution(2560, 1440, true);
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
                 break;
             case 2:
-                Screen.SetResolution(3840, 2160, true);
+                Screen.SetResolution(3840, 2160, Screen.fullScreen);
                 break;
         }
     }
